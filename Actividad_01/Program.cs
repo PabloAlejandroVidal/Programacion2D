5d61c6ebeab6a78904e261e7c97bfc4e8f19b536
Pablo Alejandro Vidal <pabloalejandrovidal@gmail.com>
2022-08-27 17:47:48 -0300

    Progrmacion_2D

---
+++ Actividad_01/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Actividad01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValorIngresado;
+            int maxValorIngresado;
+            int sumaValorIngresado;
+            int promedioValorIngresado;
+            int valorIngresado;
+            sumaValorIngresado = 0;
+
+            Console.WriteLine("Ingrese un valor");
+            while (!(int.TryParse(Console.ReadLine(), out valorIngresado)))
+            {
+                Console.WriteLine("Ingrese un valor valido");
+
+            }
+            minValorIngresado = valorIngresado;
+            maxValorIngresado = valorIngresado;
+            sumaValorIngresado += valorIngresado;
+
+            for(int i = 0; i < 4; i++){
+                Console.WriteLine("Ingrese un valor");
+                while (!(int.TryParse(Console.ReadLine(), out valorIngresado)))
+                {
+                    Console.WriteLine("Ingrese un valor valido");
+                }
+                if (valorIngresado < minValorIngresado)
+                {
+                    minValorIngresado = valorIngresado;
+                }
+                if (valorIngresado > maxValorIngresado)
+                {
+                    maxValorIngresado = valorIngresado;
+                }
+                sumaValorIngresado += valorIngresado;
+            }
+
+            promedioValorIngresado = (sumaValorIngresado / 5);
+
+            Console.WriteLine($"El valor minimo es {minValorIngresado}");
+            Console.WriteLine($"El valor maximo es {maxValorIngresado}");
+            Console.WriteLine($"El valor promedio es {promedioValorIngresado}");
+
+        }
+    }
+}
---
+++ Actividad_02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Actividad02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int cuadradoDelNumeroIngresado;
+            int cuboDelNumeroIngresado;
+            Console.WriteLine("Ingresa un numero");
+            while (!int.TryParse(Console.ReadLine(), out numeroIngresado))
+            {
+                Console.WriteLine("Ingresa un numero correcto");
+            }
+            if(numeroIngresado > 0)
+            {
+                cuadradoDelNumeroIngresado = (int)Math.Pow(numeroIngresado, 2);
+                cuboDelNumeroIngresado = (int)Math.Pow(numeroIngresado, 3);
+                Console.WriteLine($"El cuadrado de {numeroIngresado} es: {cuadradoDelNumeroIngresado}");
+                Console.WriteLine($"El cubo de {numeroIngresado} es: {cuboDelNumeroIngresado}");
+            }
+
+
+        }
+    }
+}
