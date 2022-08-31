69d53f4903dc351d9db3798d31143e9ca0f6ce4b
Pablo Alejandro Vidal <pabloalejandrovidal@gmail.com>
2022-08-31 12:20:11 -0300

    Programacion_2D

---
+++ EjerEncap1/EjerEncap1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+using System;
+
+namespace EjerEncap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Perez", "Agustin", 12345);
+            Alumno alumno2 = new Alumno("Garcia", "Jorge", 12346);
+            Alumno alumno3 = new Alumno("Lopez", "Pablo", 12347);
+
+            alumno1.Nota1 = 8;
+            alumno2.Nota1 = 7;
+            alumno3.Nota1 = 9;
+
+            alumno1.Nota2 = 7;
+            alumno2.Nota2 = 9;
+            alumno3.Nota2 = 8;
+
+            int notaFinal1 = alumno1.CalcularNotaFinal();
+            int notaFinal2 = alumno2.CalcularNotaFinal();
+            int notaFinal3 = alumno3.CalcularNotaFinal();
+
+            Console.WriteLine("El alumno: " + alumno1.Apellido + ", " + alumno1.Nombre + " tiene como nota final: " + notaFinal1);
+            Console.WriteLine("El alumno: " + alumno2.Apellido + ", " + alumno2.Nombre + " tiene como nota final: " + notaFinal2);
+            Console.WriteLine("El alumno: " + alumno3.Apellido + ", " + alumno3.Nombre + " tiene como nota final: " + notaFinal3);
+        }
+    }
+}
---
+++ EjerEncap1/Entidades/Alumno.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+        private int nota1;
+        private int nota2;
+        private string apellido;
+        private string nombre;
+        private int legajo;
+        public Alumno(string apellido, string nombre, int legajo)
+        {
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.legajo = legajo;
+        }
+
+        public int Nota1
+        {
+            get 
+            {
+                return nota1;
+            }
+            
+            set
+            {
+                if(value >= 0 && value <= 10)
+                {
+                    nota1 = value;
+                }
+                else
+                {
+                    nota1 = 0;
+                }
+            }
+        }
+
+        public int Nota2
+        {
+            get
+            {
+                return nota2;
+            }
+
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    nota2 = value;
+                }
+                else
+                {
+                    nota2 = 0;
+                }
+            }
+        }
+
+        public string Apellido
+        { 
+            get {
+                return apellido;
+            }
+        }
+        public string Nombre
+        {
+            get {
+                return nombre;
+            }
+        }
+        public int Legajo
+        {
+            get {
+                return legajo;
+            }
+        }
+
+        public int CalcularNotaFinal()
+        {
+            if (nota1 > 4 && nota2 > 4)
+            {
+                return (nota1 + nota2) / 2; 
+            }
+            else
+            {
+                return 2; 
+            }
+        }
+    }
+}
